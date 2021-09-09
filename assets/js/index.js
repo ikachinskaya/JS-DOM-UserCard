67b2c0b2b2e70184da786e723a8d62816bc79ac9
Inna <inna.kachinskaya13@gmail.com>
2021-09-09 18:49:15 +0300

    Add users.json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const cardsContainer = document.querySelector("#root");
-const dataJson = fetch("http://192.168.1.149:3000/api/users")
+const dataJson = fetch("./assets/js/users.json")
   .then((response) => response.json())
   .then((dataJson) => {
     const userCards = dataJson.map((user) => generateUserCard(user));
-    cardsContainer.append(...userCards);   
+    cardsContainer.append(...userCards);
+  })
+  .catch((err) => {
+    throw new Error("Error");
   });
+//"http://192.168.1.149:3000/api/users"
