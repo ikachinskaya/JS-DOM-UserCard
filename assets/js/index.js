69d28c02905f93fdf697872039fec1786137bc38
Inna <inna.kachinskaya13@gmail.com>
2021-09-09 16:50:59 +0300

    Add JSON

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,14 @@
 "use strict";
-const cardsContainer = document.querySelector("#root");
 
-const userCards = data.map((user) => generateUserCard(user));
-cardsContainer.append(...userCards);
+const cardsContainer = document.querySelector("#root");
+const dataJson = fetch("http://192.168.1.149:3000/api/users")
+  .then((response) => response.json())
+  .then((dataJson) => {
+    const userCards = dataJson.map((user) => generateUserCard(user));
+    cardsContainer.append(...userCards);   
+  });
+
+  
+
+// const userCards = data.map((user) => generateUserCard(user));
+ 
