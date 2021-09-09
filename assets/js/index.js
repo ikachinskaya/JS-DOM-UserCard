c234287f745bc7c0be85cb6c8800179005d4a6cb
Inna <inna.kachinskaya13@gmail.com>
2021-09-09 16:55:18 +0300

    Fix index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,8 +7,3 @@ const dataJson = fetch("http://192.168.1.149:3000/api/users")
     const userCards = dataJson.map((user) => generateUserCard(user));
     cardsContainer.append(...userCards);   
   });
-
-  
-
-// const userCards = data.map((user) => generateUserCard(user));
- 
